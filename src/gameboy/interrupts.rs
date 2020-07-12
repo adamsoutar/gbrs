e45ab986f1d16a3e57c35cdf29af5144e61dcd8d
Adam <adam@overflo.me>
2020-07-12 01:47:52 +0100

    Actually pay attention to what's written to Interrupts

--- src/gameboy/interrupts.rs
+++ src/gameboy/interrupts.rs
@@ -7,9 +7,22 @@ pub struct InterruptFields {
 }
 
 impl InterruptFields {
+    // TODO: Check these go in this order and not backwards
     pub fn serialise (&self) -> u8 {
-        // TODO
-        0
+        let b1 = self.v_blank as u8;
+        let b2 = (self.lcd_stat as u8) << 1;
+        let b3 = (self.timer as u8) << 2;
+        let b4 = (self.serial as u8) << 3;
+        let b5 = (self.joypad as u8) << 4;
+        b1 | b2 | b3 | b4 | b5
+    }
+
+    pub fn deserialise(&mut self, n: u8) {
+        self.v_blank = (n & 1) == 1;
+        self.lcd_stat = ((n >> 1) & 1) == 1;
+        self.timer = ((n >> 2) & 1) == 1;
+        self.serial = ((n >> 3) & 1) == 1;
+        self.joypad = ((n >> 4) & 1) == 1;
     }
 
     // TODO: Check if these actuall do all start false
@@ -36,12 +49,12 @@ impl Interrupts {
     // TODO: These
     // Called when GB writes to FFFF
     pub fn enable_write (&mut self, value: u8) {
-
+        self.enable.deserialise(value)
     }
 
     // Called when GB writes to FF0F
     pub fn flag_write (&mut self, value: u8) {
-
+        self.flag.deserialise(value)
     }
 
     // Called when GB reads from FFFF
