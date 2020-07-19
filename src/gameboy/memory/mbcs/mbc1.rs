3a3e9fc8a2504ff1a5ce2eb32e70e93c7d46f111
adamsoutar <adam@overflo.me>
2020-07-19 22:04:35 +0100

    MBC1 fixes

--- src/gameboy/memory/mbcs/mbc1.rs
+++ src/gameboy/memory/mbcs/mbc1.rs
@@ -12,6 +12,8 @@ pub struct MBC1 {
 
     // Not really supported yet
     pub ram: Ram,
+    pub ram_enabled: bool,
+
     pub battery: bool
 }
 
@@ -26,22 +28,25 @@ impl MBC for MBC1 {
 
     fn write(&mut self, address: u16, value: u8) {
         match address {
-            2000 ..= 0x3FFF => {
+            0x0000 ..= 0x1FFF => {
+                self.ram_enabled = (value & 0x0A) == 0x0A;
+            },
+            0x2000 ..= 0x3FFF => {
                 // TODO: More advanced banking support
                 let mut n = value & 0b11111;
                 if n == 0 { n = 1 }
                 self.rom_bank = n
             },
-            _ => println!("Unsupported MBC1 write at {:#06x}", address)
+            _ => println!("Unsupported MBC1 write at {:#06x} (value: {:#04x})", address, value)
         }
     }
 
-    fn ram_read(&self, address: u16) -> u8 {
+    fn ram_read(&self, _: u16) -> u8 {
         // TODO
         0
     }
 
-    fn ram_write(&mut self, address: u16, value: u8) {
+    fn ram_write(&mut self, _: u16, _: u8) {
         // TODO
     }
 }
@@ -58,6 +63,7 @@ impl MBC1 {
         // TODO: Support the RAM
         MBC1 {
             rom,
+            ram_enabled: false,
             rom_bank: 1,
             ram: Ram::new(cart_info.ram_size),
             battery: cart_info.cart_type == 0x03
