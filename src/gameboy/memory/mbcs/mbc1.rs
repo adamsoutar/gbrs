4de1d5bf746376191579895c509f21e25783d09f
adamsoutar <adam@overflo.me>
2022-01-01 21:08:02 +0000

    MBC1 RAM fixes This setup makes both Pac-Man and Super Mario Land 2 work at the same time

--- src/gameboy/memory/mbcs/mbc1.rs
+++ src/gameboy/memory/mbcs/mbc1.rs
@@ -31,22 +31,32 @@ impl MBC for MBC1 {
                 self.ram_enabled = (value & 0x0A) == 0x0A;
             },
             0x2000 ..= 0x3FFF => {
-                // TODO: More advanced banking support
+                // TODO: Bank numbers are masked to the max bank number
+                // TODO: Upper/RAM banking support
                 let mut n = value & 0b11111;
                 if n == 0 { n = 1 }
                 self.rom_bank = n
             },
-            // Frogger writes 0 to 0x408e constantly (so much that the below
-            //   println slows down the game). Maybe a bug in that game?
-            _ => {} //println!("Unsupported MBC1 write at {:#06x} (value: {:#04x})", address, value)
+            // 0x4000 ..= 0x5FFF => {
+            //     panic!("Unsupported upper bank number or RAM banking in MBC1")
+            // },
+            // 0x6000 ..= 0x7FFF => {
+            //     panic!("Unsupported MBC1 mode select write")
+            // },
+            _ => {}//panic!("Unsupported MBC1 write at {:#06x} (value: {:#04x})", address, value)
         }
     }
 
     fn ram_read(&self, address: u16) -> u8 {
         if !self.ram_enabled && !self.has_shown_ram_warning {
             println!("[WARN] MBC1 RAM read while disabled");
-            // return 0
         }
+
+        // When an address outside of RAM space is read, the gameboy
+        // doesn't seem to be intended to crash.
+        // Not sure what to return here, but unusable RAM on the GB itself
+        // returns 0xFF
+        if address as usize >= self.ram.size { return 0xFF }
 
         self.ram.read(address)
     }
@@ -56,8 +66,10 @@ impl MBC for MBC1 {
             println!("[WARN] MBC1 RAM write while disabled");
             // Otherwise the game is slowed down by constant debug printing
             self.has_shown_ram_warning = true;
-            // return
         }
+
+        // See note in ram_read
+        if address as usize >= self.ram.size { return }
 
         self.ram.write(address, value)
     }
