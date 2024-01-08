073a7b1c18a3dfcff871c422b9582b8bb32122aa
Adam Soutar <me@aso.sh>
2024-01-08 01:51:59 +0000

    Port to experimental HEAD version of libretro-rs In hopes this will fix core info issues

--- libretro/src/lib.rs
+++ libretro/src/lib.rs
@@ -1,92 +1,106 @@
 use gbrs_core::constants::*;
 use gbrs_core::cpu::Cpu;
-use libretro_rs::*;
+use libretro_rs::c_utf8::c_utf8;
+use libretro_rs::retro::env::{Init, UnloadGame};
+use libretro_rs::retro::pixel::{Format, XRGB8888};
+use libretro_rs::retro::*;
+use libretro_rs::{ext, libretro_core};
 
 struct LibretroCore {
-    gameboy: Option<Cpu>,
-    last_rom_data: Vec<u8>
+    gameboy: Cpu,
+    last_rom_data: Vec<u8>,
+    rendering_mode: SoftwareRenderEnabled,
+    frame_buffer: [XRGB8888; SCREEN_WIDTH * SCREEN_HEIGHT],
+    pixel_format: Format<XRGB8888>,
 }
 
-impl LibretroCore {
+impl<'a> Core<'a> for LibretroCore {
+    type Init = ();
 
-}
-
-const SCREEN_BYTES_SIZE: usize = SCREEN_WIDTH * SCREEN_HEIGHT * 2;
-
-impl RetroCore for LibretroCore {
-    fn init(_: &RetroEnvironment) -> Self {
-        LibretroCore {
-            gameboy: None,
-            last_rom_data: vec![]
-        }
+    fn get_system_info() -> SystemInfo {
+        SystemInfo::new(
+            c_utf8!("gbrs"),
+            c_utf8!(env!("CARGO_PKG_VERSION")),
+            ext!["gb","gbc"]
+        )
     }
 
-    fn get_system_info() -> RetroSystemInfo {
-        let info = RetroSystemInfo::new("gbrs", env!("CARGO_PKG_VERSION"));
-        info.with_valid_extensions(&vec!["gb", "gbc"])
+    fn init(_env: &mut impl Init) -> Self::Init {
+      ()
     }
 
-    fn reset(&mut self, _: &libretro_rs::RetroEnvironment) {
-        self.gameboy = Some(Cpu::from_rom_bytes(self.last_rom_data.clone()));
+    fn get_system_av_info(&self, _env: &mut impl env::GetAvInfo) -> SystemAVInfo {
+        SystemAVInfo::new(
+            GameGeometry::fixed(SCREEN_WIDTH as u16, SCREEN_HEIGHT as u16),
+            SystemTiming::new(DEFAULT_FRAME_RATE as f64, SOUND_SAMPLE_RATE as f64)
+        )
     }
 
-    fn run(&mut self, environment: &libretro_rs::RetroEnvironment, runtime: &RetroRuntime) {
-        let gb = self.gameboy.as_mut().unwrap();
+    fn run(&mut self, _env: &mut impl env::Run, runtime: &mut impl Callbacks) -> InputsPolled {
+        let gb = &mut self.gameboy;
 
-        // Convert frame into 0RGB1555
-        let mut screen_bytes = [0; SCREEN_BYTES_SIZE];
         for i in 0..SCREEN_BUFFER_SIZE {
+            let mut pixel: u32 = 0;
             let r = gb.gpu.finished_frame[i].red;
             let g = gb.gpu.finished_frame[i].green;
             let b = gb.gpu.finished_frame[i].blue;
-            let r5 = (r >> 3) as u16;
-            let g5 = (g >> 3) as u16;
-            let b5 = (b >> 3) as u16;
-            let final_colour = (r5 << 10) | (g5 << 5) | b5;
-            screen_bytes[i * 2] = (final_colour & 0xFF) as u8;
-            screen_bytes[i * 2 + 1] = ((final_colour >> 8) & 0xFF) as u8;
+            pixel |= r as u32;
+            pixel |= (g as u32) << 8;
+            pixel |= (b as u32) << 16;
+            self.frame_buffer[i] = XRGB8888::new_with_raw_value(pixel);
         }
 
-        runtime.upload_video_frame(&screen_bytes, SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32, SCREEN_WIDTH * 2);
+        let frame = Frame::new(&self.frame_buffer, SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32);
+        runtime.upload_video_frame(&self.rendering_mode, &self.pixel_format, &frame);
         runtime.upload_audio_frame(&gb.mem.apu.buffer);
 
-        gb.mem.joypad.a_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::A);
-        gb.mem.joypad.b_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::B);
-        gb.mem.joypad.start_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::Start);
-        gb.mem.joypad.select_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::Select);
-        gb.mem.joypad.left_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::Left);
-        gb.mem.joypad.right_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::Right);
-        gb.mem.joypad.up_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::Up);
-        gb.mem.joypad.down_pressed = runtime.is_joypad_button_pressed(0, RetroJoypadButton::Down);
+        let inputs_polled = runtime.poll_inputs();
+        let port = DevicePort::new(0);
+        gb.mem.joypad.a_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::A);
+        gb.mem.joypad.b_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::B);
+        gb.mem.joypad.start_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::Start);
+        gb.mem.joypad.select_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::Select);
+        gb.mem.joypad.left_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::Left);
+        gb.mem.joypad.right_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::Right);
+        gb.mem.joypad.up_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::Up);
+        gb.mem.joypad.down_pressed = runtime.is_joypad_button_pressed(port, JoypadButton::Down);
 
         while !gb.mem.apu.buffer_full {
             gb.step();
         }
         gb.mem.apu.buffer_full = false;
+
+        inputs_polled
     }
 
-    fn load_game(&mut self, environment: &libretro_rs::RetroEnvironment, game: RetroGame<'_>) -> RetroLoadGameResult {
-        match &game {
-            RetroGame::None { meta: _ } => {
-                panic!("Asked to load RetroGame::None")
-            },
-            RetroGame::Data { meta: _, data } => {
-                self.last_rom_data = data.to_vec();
-                self.gameboy = Some(Cpu::from_rom_bytes(data.to_vec()));
-                environment.set_pixel_format(RetroPixelFormat::XRGB8888);
-                RetroLoadGameResult::Success {
-                    audio: RetroAudioInfo::new(SOUND_SAMPLE_RATE as f64),
-                    video: RetroVideoInfo::new(
-                        DEFAULT_FRAME_RATE as f64,
-                        SCREEN_WIDTH as u32,
-                        SCREEN_HEIGHT as u32)
-                }
-            },
-            RetroGame::Path { meta: _, path: _ } => {
-                panic!("Asked to load RetroGame::Path")
-            }
-        }
+    fn load_game<E: env::LoadGame>(
+      game: &GameInfo,
+      args: LoadGameExtraArgs<'a, '_, E, Self::Init>,
+    ) -> Result<Self, CoreError> {
+      let LoadGameExtraArgs {
+        env,
+        pixel_format,
+        rendering_mode,
+        ..
+      } = args;
+      let pixel_format = env.set_pixel_format_xrgb8888(pixel_format)?;
+      let data: &[u8] = game.as_data().ok_or(CoreError::new())?.data();
+      Ok(Self {
+        rendering_mode,
+        pixel_format,
+        gameboy: Cpu::from_rom_bytes(data.to_vec()),
+        last_rom_data: data.to_vec(),
+        frame_buffer: [XRGB8888::DEFAULT; SCREEN_WIDTH * SCREEN_HEIGHT],
+      })
+    }
+
+    fn reset(&mut self, _env: &mut impl env::Reset) {
+      self.gameboy = Cpu::from_rom_bytes(self.last_rom_data.clone());
+    }
+
+    fn unload_game(self, _env: &mut impl UnloadGame) -> Self::Init {
+      ()
     }
 }
 
-libretro_core!(LibretroCore);
+libretro_core!(crate::LibretroCore);
